7751cacc99e328920cd4bf4bf4936988dff6e5d6
esther <esthersant.edu@gmail.com>
2025-05-15 17:26:54 -0300

    changes build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="164782487430.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t aws-posts .
-docker tag aws-posts:latest $ECR_REGISTRY/aws-posts:latest
-docker push $ECR_REGISTRY/aws-posts:latest
+docker build -t aws-aula-posts .
+docker tag aws-aula-posts:latest $ECR_REGISTRY/aws-aula-posts:latest
+docker push $ECR_REGISTRY/aws-aula-posts:latest
